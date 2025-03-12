d1740988fe06e8c9260aad23119c4d1742620bc2
Christian Kirschberg <ckirschberg@gmail.com>
2025-03-12 17:02:30 +0100

    solution

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ function askWhatIsYourName() {
     alert("Hello " + name)
 }
 
+const pi = 5;
+
+let p1 = '5';
+let p2 = '3';
+let age = 18
+let isLegalAge = age >= 18;
+
+console.log(isLegalAge);
+
+
+
+let studentName = 'Malene';
+studentName = 'Målebånd';
+
+console.log(studentName)
+
 // askWhatIsYourName()
 
 // alert("Hello World")
